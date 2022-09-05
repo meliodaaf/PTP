038158b7176c248223c182d3595df7c150fe0a93
Clarence Subia <subiaclarence96@gmail.com>
2022-09-05 20:30:06 +0800

    added no ping

--- Information Gathering/host-discover.sh
+++ Information Gathering/host-discover.sh
@@ -5,21 +5,26 @@ MASK=$2
 
 function find_alive_hosts {
 # Ping sweep
-    fping -a -g $IP/$MASK 2>/dev/null | tee OUTPUTS/$IP-alive-hosts.txt
+    fping -a -g $IP/$MASK 2>/dev/null | tee OUTPUTS/$IP-ALIVE-HOSTS
+}
+
+function no_ping {
+    nmap -n -sn -PS22,135,443,445 $IP/$MASK -oG OUTPUTS/$IP-NO-PING-SCAN
+    cat OUTPUTS/$IP-NO-PING-SCAN | grep up | awk ' {print $2 } ' > OUTPUTS/$IP-ALL-HOSTS
 }
 
 function dns_discovery {
 # Find IPs with port 53 open
     nmap -sS -sU -p53 -n $IP/$MASK -oG OUTPUTS/$IP-NMAP-DNS
-    cat OUTPUTS/$IP-dns-servers | grep open | awk ' {print $2} ' > OUTPUTS/$IP-DNS-Servers.txt
+    cat OUTPUTS/$IP-NMAP-DNS | grep open | awk ' {print $2} ' > OUTPUTS/$IP-DNS-SERVERS
 }
 
 function nmap_scan {
 # NMAP Scan each IP grom output.txt
 
-for host in $(cat OUTPUTS/$IP-alive-hosts.txt)d
+for host in $(cat OUTPUTS/$IP-ALIVE-HOSTS)
 do
-    nmap -sS $host -oN OUTPUTS/$host-scan
+    nmap -A -T4 $host -oN OUTPUTS/$host-ALL-HOSTS
 done
 
 }

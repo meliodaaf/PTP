31aab892140d7f8a75942a624c0239cf165fe91e
Clarence Subia <subiaclarence96@gmail.com>
2022-09-05 20:54:58 +0800

    fix output

--- Information Gathering/host-discover.sh
+++ Information Gathering/host-discover.sh
@@ -25,7 +25,7 @@ function dns_discovery {
 function nmap_scan {
 # NMAP Scan each IP grom output.txt
 echo -e "[*] Scanning IPs:\n"
-echo "$(cat OUTPUTS/$IP-ALIVE-HOSTS)\n"
+echo -e "$(cat OUTPUTS/$IP-ALIVE-HOSTS)\n"
 for host in $(cat OUTPUTS/$IP-ALIVE-HOSTS)
 do
     nmap -A -T4 $host -oN OUTPUTS/$host-ALL-HOSTS
